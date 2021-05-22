a5b517003a863ecd2e8950d541b93d804a7f5423
ridolickid <84444440+ridolickid@users.noreply.github.com>
2021-05-22 15:40:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,40 +3,24 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot if you like :D
-
-Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
-
-So no need to add filters for your files or movies from now on!
-
+ഇത് @Movievibezz നു വേണ്ടി ഉണ്ടാക്കിയ ഒരു Auto-Filter Bot ആണ്. 
+Join Fast 
+@Movievibezz
 For more click <i>help</i></b>"""
 
 
-    HELP_MSG = """<b>How to use the bot??</b>
-<i>    
-* Add user account to your channel !
-
-* Add bot to group with admin rights.
-
-* Once deployed and bot is started, send a message to channel (must do step)
-
-* Bot will work now :)
-
-No need add each filter again!
-Bot will automatically search for your file and give links to that!
-</i>
-
-<b>© @TroJanzHEX</b>"""
+    HELP_MSG = """<b>®®</b>
+<
+<b>© @Movievibezz</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Ridolickid
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
