31a7ebdaa8e155e3a0ee5702bcbb365ba964fba6
ridolickid <84444440+ridolickid@users.noreply.github.com>
2021-05-23 20:57:23 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,11 +6,11 @@ class script(object):
 ഇത് @Movievibezz നു വേണ്ടി ഉണ്ടാക്കിയ ഒരു Auto-Filter Bot ആണ്. 
 Join Fast 
 @Movievibezz
-For more click <i>help</i></b>"""
+<i>help</i></b>"""
 
 
-    HELP_MSG = """<b>®®</b>
-<
+    HELP_MSG = """<b>sorry</b>
+
 <b>© @Movievibezz</b>"""
 
 
@@ -20,7 +20,7 @@ For more click <i>help</i></b>"""
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Group  :</b> <a @Movievibezz </a> 
 
-
+Join here
 """
